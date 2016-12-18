9f19dc8f786a18d2fb7d804dd79bc1424ff997fb
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-19 00:52:27 +0700

    1

---
+++ collect-from-docker.sh
@@ -0,0 +1,36 @@
+﻿#! /bin/bash
+# build docker image
+tag=$(uuidgen)
+echo $tag
+docker build -t $tag --force-rm=true .
+docker run $tag apt-get install -y \
+  liblept4 \
+  libleptonica-dev \
+  tesseract-ocr \
+  tesseract-ocr-dev
+DCID=$(docker ps -l -q)
+
+echo $DCID
+
+# get list of changed files in container
+libfiles=$(docker diff $DCID|cut -f2 -d" "|grep -i -e "\.so]$" -e "\.a$")
+echo $libfiles
+
+# clean previous installation
+rm -rf vendor
+mkdir -p vendor/tesseract-ocr
+mkdir -p vendor/leptonica
+
+# copy header files
+docker cp $DCID:/usr/include/tesseract-ocr vendor
+docker cp $DCID:/usr/include/leptonica vendor
+docker cp $DCID:/usr/bin/tesseract-ocr vendor/tesseract-ocr
+
+echo $libfiles
+
+for libfilename in $libfiles
+do
+  echo "Copying $libfilename.."
+  docker cp $DCID:$libfilename vendor/tesseract-ocr
+  echo "OK"
+done
